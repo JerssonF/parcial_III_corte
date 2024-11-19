95ad5c4ea3b0d442f229403559e4b138a7c8e8de
Jersson Fabián Buitrago Murcia <166882886+JerssonF@users.noreply.github.com>
2024-11-19 07:53:50 -0500

    back

--- Biblioteca-Atena-main/BackendAtena/src/main/java/com/corhuila/BackendAtena/IRepository/ILibrosRepository.java
+++
@@ -1,7 +0,0 @@
-package com.corhuila.BackendAtena.IRepository;
-
-import com.corhuila.BackendAtena.Entity.Libros;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ILibrosRepository extends JpaRepository<Libros, Integer> {
-}
--- Biblioteca-Atena-main/BackendAtena/src/main/java/com/corhuila/BackendAtena/IRepository/IPrestamosRepository.java
+++
@@ -1,7 +0,0 @@
-package com.corhuila.BackendAtena.IRepository;
-
-import com.corhuila.BackendAtena.Entity.Prestamos;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface IPrestamosRepository extends JpaRepository<Prestamos, Integer> {
-}
